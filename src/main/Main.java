623e13588c62ad3497a68c7fe40267368da3d17e
LWK <jmlee7100000000@gmail.com>
2022-04-11 12:16:55 +0900

    팀장 커밋 완료

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		System.out.println("로컬 저장소 사용");
 		System.out.println("원격저장소 사용가능");
 		System.out.println("원격저장소 사용가능2");
+		System.out.println("팀장 코드 시작입니다.");
+		System.out.println("팀장 코드 마지막 입니다.");
 	}
 
 }
