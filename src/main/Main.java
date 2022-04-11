674306b86f4ddcb7b77400ea9a11de11ce144f47
LWK <jmlee7100000000@gmail.com>
2022-04-11 12:25:27 +0900

    팀장새코드완료

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,9 @@ public class Main {
 		System.out.println("원격저장소 사용가능2");
 		System.out.println("팀장 코드 시작입니다.");
 		System.out.println("팀장 코드 마지막 입니다.");
+		
+		System.out.println("팀장 새코드시작");
+		System.out.println("팀장 새코드끝");
 	}
 
 }
